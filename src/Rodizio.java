f40ef92c6068fdb7b2f29ddf02dc7870a269dea0
Eliane Marion <pf1551@fiap.com.br>
2023-11-08 18:56:33 -0300

    Aula 03 - Operadores lógicos e switch

---
+++ src/Rodizio.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Rodizio {
+
+    public static void main(String[] args) {
+        /*Ler os números referente a placa de um carro xxx-9999
+         * exibir usando o switch o dia do rodizio de veículos.
+         * 1 e 2 - segunda-feira
+         * 3 e 4 - terça
+         * 5 e 6 - quarta
+         * 7 e 8 - quinta
+         * 9 e 0 - sexta */
+
+        Scanner leitor = new Scanner(System.in);
+        int placa, finalPlaca;
+
+        System.out.println("Digite os números da placa do seu automóvel:");
+        placa = leitor.nextInt();
+    }
+}
